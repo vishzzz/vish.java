bae61e611d138ca52ad2c103dee668dcdb1f5446
VISHNU V <86774908+vishzzz@users.noreply.github.com>
2023-07-12 11:23:00 +0530

    Add files via upload

---
+++ java/Looping constructs.java
@@ -0,0 +1,27 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+class GFG
+{
+	// Prints numbers from 1 to n
+	static void printNos(int n)
+	{
+		if(n > 0)
+		{
+			printNos(n - 1);
+			System.out.print(n + " ");
+		}
+		return;
+	}
+
+	// Driver Code
+	public static void main(String[] args)
+	{
+		printNos(100);
+	}
+}
+
+// This code is contributed by Manish_100
